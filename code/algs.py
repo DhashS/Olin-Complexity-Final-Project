9d3005371e63135329f688e27b0935e8585f2e79
dhash <dhash@Hypernova-1ML-Iron.olin.edu>
2016-11-18 04:40:11 -0500

    Implemented the base greedy agent algorithm

---
+++ code/algs.py
@@ -0,0 +1,27 @@
+import networkx as nx
+import numpy as np
+
+def simple_greed(p, n):
+    """Takes p and produces a tour from a start node n by choosing
+    the node with the lowest weight that it hasn't already visited.
+    
+    p :: [TSP object]
+    n :: [Integer] Start node
+    
+    returns: tour of p
+    """
+    seen = set()
+    current_node = n
+    seen.add(current_node)
+    p_nodes = set(p.graph.nodes())
+    
+    cost = 0
+
+    while p_nodes.difference(seen):
+        unseen_neighbors = [n for n in p.graph.neighbors(current_node) if n not in seen]
+        near_nodes = sorted(unseen_neighbors, key=lambda x: p.graph.get_edge_data(current_node, x)['weight'])
+        closest_node = near_nodes[0]
+        cost += p.graph.get_edge_data(current_node, closest_node)['weight']
+        seen.add(closest_node)
+    return cost
+        
